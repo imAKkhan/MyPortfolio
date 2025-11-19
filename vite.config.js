7cd6d1046f0f00e5a5e9921a26a401a80e2c71ee
imAKkhan <ashar8669@gmail.com>
2025-11-19 11:22:48 +0530

    Updated vite config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   // eslint-disable-next-line no-undef
-  base: process.env.VITE_BASE_PATH || "/MyPortfolio/", 
+  base: process.env.VITE_BASE_PATH || "", 
 });
