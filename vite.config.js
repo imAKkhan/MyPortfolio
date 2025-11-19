7b275c6f9db4fcb1da9154a655041d0f4c9ae3ff
imAKkhan <ashar8669@gmail.com>
2025-11-19 10:45:09 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-})
+  plugins: [react(), tailwindcss()],
+  // eslint-disable-next-line no-undef
+  base: process.env.VITE_BASE_PATH || "/MyPortfolio/", 
+});
